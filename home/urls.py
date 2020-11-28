b755fe94631a4feeb23038b01230001dd0766ad6
nguyentienanhvuongchuse <nguyenvuong.09062000@gmail.com>
2020-11-28 10:19:59 +0700

    template

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("contact/", views.contact, name = "contact"),
+    path("order/", views.order, name = "order"),
+    path("detail/", views.detail, name = "detail"),
     path("login/", views.login, name = "login"),
     path("signup/", views.signup, name = "signup")
 ]
