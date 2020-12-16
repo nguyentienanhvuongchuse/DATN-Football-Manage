b34b8b1e99321150c57aa99556aa4795f8e33a04
nguyentienanhvuongchuse <nguyenvuong.09062000@gmail.com>
2020-12-16 21:40:43 +0700

    CRUD yard

--- yard/urls.py
+++ yard/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("booking", views.booking, name="Booking"),
+    path("booking", views.booking, name="manage_booking"),
     path("location", views.manage_location, name="manage_location"),
     path("yard", views.manage_yard, name="manage_yard"),
+    path("newyard/", views.create_yard, name="create_yard"),
+    path("detail/<pk>/", views.update_yard, name="update_yard"),
+    path("delete_yard/<pk>/", views.delete_yard, name="delete_yard"),
+    path("time/<pk>/", views.time_cost, name="update_time"),
     path("statistical", views.statistical, name="statistical")
 ]
--- yard/views.py
+++ yard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import LocationForm
+from django.forms import inlineformset_factory
+from .forms import *
 from .models import *
 # Create your views here.
 
@@ -9,7 +10,6 @@ def booking(request):
 
 def manage_location(request):
     current_user = request.user.id
-    print(current_user)
     location = Location.objects.get(user=current_user)
 
     form = LocationForm(instance=location)
@@ -22,7 +22,60 @@ def manage_location(request):
     return render(request, "manage/location.html", context)
 
 def manage_yard(request):
-    return HttpResponse("Admin Yard")
+    current_user = request.user.id
+    location_id = Location.objects.get(user=current_user)
+    yard = Yard.objects.filter(location=location_id)
+    context = {"yard":yard}
+    return render(request, "manage/yard.html", context)
+
+def update_yard(request,pk):
+    yard = Yard.objects.get(id=pk)
+    if request.method == "POST":
+        form = YardForm(request.POST, instance=yard)
+        if form.is_valid():
+            form.save()
+            return redirect("manage_yard")
+    else:
+        form = YardForm(instance=yard)
+    context = {"form":form, "yard":yard}
+    return render(request, "manage/form_yard.html", context)
+
+def delete_yard(request,pk):
+    yard = Yard.objects.get(id=pk)
+    if request.method == "POST":
+        yard.delete()
+        return redirect("manage_yard")
+    context = {"yard":yard}
+    return render(request, "manage/delete_yard.html", context)
+
+def create_yard(request):
+    current_user = request.user.id
+    location = Location.objects.get(user=current_user)
+    form = CreateYardForm(initial={"location":location})
+    if request.method == "POST":
+        form = CreateYardForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("manage_yard")
+    else:
+        form = CreateYardForm(initial={"location":location})
+    context = {"form":form}
+    return render(request, "manage/create_yard.html", context)
+
+def time_cost(request,pk):
+    TimeFormSet = inlineformset_factory(Yard, Time,fields=("time","cost"),extra=5)
+    yard = Yard.objects.get(id=pk)
+    time = Time.objects.filter(yard=pk)
+
+    formset = TimeFormSet(queryset=Time.objects.none(), instance=yard)
+    if request.method == "POST":
+        form = TimeCostForm(request.POST)
+        formset = TimeFormSet(request.POST, instance=yard)
+        if formset.is_valid():
+            formset.save()
+            return redirect("manage_yard")
+    context = {"time":time, "form":formset}
+    return render(request, "manage/time.html",context)
 
 def statistical(request):
     return HttpResponse("Statistical")
